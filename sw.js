cadff8a582a3e026907a665d5c60d232f2ba49bd
bakhtiyarbhai03-cmyk <bakhtiyarbhai03@gmail.com>
2026-06-13 01:49:42 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'chess-master-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.svg',
+  './stockfish.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
